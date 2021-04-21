e5edee643b823003e7215f073cc0662513aab588
asha joseph <ashajo183@gmail.com>
2021-04-21 09:09:35 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/registeration_student/RegisterationStudentApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.registeration_student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterationStudentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegisterationStudentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/registeration_student/RegisterationStudentApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.registeration_student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterationStudentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
