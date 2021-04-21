33e62a9d1a135bc586844934fcd0e10ea4fb570a
asha joseph <ashajo183@gmail.com>
2021-04-21 10:48:54 +0200

    Defined the functionality "findById"

--- src/main/java/se/lexicon/registeration_student/controller/StudentController.java
+++ src/main/java/se/lexicon/registeration_student/controller/StudentController.java
@@ -3,12 +3,14 @@ package se.lexicon.registeration_student.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import se.lexicon.registeration_student.entity.Student;
 import se.lexicon.registeration_student.repository.StudentRepository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class StudentController {
@@ -23,6 +25,14 @@ this.studentRepository=studentRepository;
         List<Student> studentList= new ArrayList<>();
         studentRepository.findAll().iterator().forEachRemaining(studentList::add);
         return ResponseEntity.ok(studentList);
+    }
+
+    @GetMapping("/api/student/{id}")
+    public ResponseEntity<Student>findById(@PathVariable("id")String id){
+    Optional<Student> optionalstudent=studentRepository.findById(id);
+    if (optionalstudent.isPresent())
+        return ResponseEntity.ok(optionalstudent.get());
+        else return ResponseEntity.noContent().build();
     }
 
 }
